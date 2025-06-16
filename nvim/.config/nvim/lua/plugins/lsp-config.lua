cbb8e9f538bfed6b84c7eb477d77959a40adca6f
Ricardo Beck <spritkopf@gmail.com>
2025-06-16 13:38:30 +0200

    add clangd and format shortcut

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -4,16 +4,16 @@ return {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     -- mason-lspconfig
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "rust_analyzer" }
+                ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "cmake" },
             })
-        end
+        end,
     },
     {
         "neovim/nvim-lspconfig",
@@ -21,11 +21,13 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.rust_analyzer.setup({})
+            lspconfig.clangd.setup({})
+            lspconfig.cmake.setup({})
 
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, {})
-        end
-
-    }
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
+        end,
+    },
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,12 +2,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {"lua", "rust", "python", "c"},
+            auto_install = true,
             highlight = { enable = true },
-            indent = { enable = true }
+            indent = { enable = true },
         })
-    end
+    end,
 }
