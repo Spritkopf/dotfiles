7212780cb8b790850456d41f5ba6cba51141a5f7
Ricardo Beck <spritkopf@gmail.com>
2025-06-16 13:38:30 +0200

    add completions & snippets

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -18,14 +18,24 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.rust_analyzer.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.cmake.setup({})
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.rust_analyzer.setup({
+                capabilities = capabilities
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities
+            })
+            lspconfig.cmake.setup({
+                capabilities = capabilities
+            })
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
             vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
             vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, {})
             vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
         end,
