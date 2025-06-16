6d3a53ae101db19e3478be8bdc241dc879ea1e58
Ricardo Beck <spritkopf@gmail.com>
2025-06-16 13:38:30 +0200

    add LSP

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -3,8 +3,7 @@ return {
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        local harpoon = require("harpoon")
-        harpoon:setup()
+        require("harpoon").setup()
         vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
