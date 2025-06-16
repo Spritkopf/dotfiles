1e5925b5692d236725bdd1fdb4be6f490e83d551
Ricardo Beck <spritkopf@gmail.com>
2025-06-16 13:38:30 +0200

    add dashboard, i3 rounded corners

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -4,3 +4,14 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.g.mapleader = " "
+vim.g.background = "light"
+
+vim.opt.swapfile = false
+
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-Up>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-Down>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-Left>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-Right>', ':wincmd l<CR>')
+
