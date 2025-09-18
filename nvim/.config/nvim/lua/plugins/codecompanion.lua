6f5e0568486715e290b8e566160e858022ea2589
Ricardo Beck <spritkopf@gmail.com>
2025-09-18 11:09:32 +0200

    nvim: codecompanion update, overseer quickfix, leader p binding in visual mode

--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -1,33 +1,35 @@
 return {
-	"olimorris/codecompanion.nvim",
-	config = function()
-		require("codecompanion").setup({
-			adapters = {
-				gemini = function()
-					return require("codecompanion.adapters").extend("gemini", {
-						env = {
-							api_key = "cmd: cat ~/secret/gemini.txt",
-						},
-					})
-				end,
-			},
-			strategies = {
-				chat = {
-					adapter = "gemini",
-				},
-				inline = {
-					adapter = "gemini",
-				},
-			},
-		})
+    "olimorris/codecompanion.nvim",
+    config = function()
+        require("codecompanion").setup({
+            adapters = {
+                http = {
+                    gemini = function()
+                        return require("codecompanion.adapters.http").extend("gemini", {
+                            env = {
+                                api_key = "cmd: cat ~/secret/gemini.txt",
+                            },
+                        })
+                    end,
+                },
+            },
+            strategies = {
+                chat = {
+                    adapter = "gemini",
+                },
+                inline = {
+                    adapter = "gemini",
+                },
+            },
+        })
         local wk = require("which-key")
         wk.add({
             { "<leader>c", group = "[C]odeCompanion" },
         })
-        vim.keymap.set("n", "<leader>cc", "<cmd>CodeCompanionChat<CR>", {desc = "[C]hat"})
-	end,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
+        vim.keymap.set("n", "<leader>cc", "<cmd>CodeCompanionChat<CR>", { desc = "[C]hat" })
+    end,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
 }
