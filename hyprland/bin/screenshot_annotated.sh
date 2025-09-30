4e67423839715e2a2cefc41c19f124dcd1581f41
Ricardo Beck <spritkopf@gmail.com>
2025-09-30 10:55:07 +0200

    use flameshot

--- hyprland/bin/screenshot_annotated.sh
+++ hyprland/bin/screenshot_annotated.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-pkill slurp || hyprshot -m ${1:-region} --raw |
-  satty --filename - \
-    --output-filename "/tmp/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png" \
-    --early-exit \
-    --actions-on-enter save-to-clipboard \
-    --copy-command 'wl-copy'
+# slurp and satty
+# disabled this for now since it itÄs not mature enough
+# pkill slurp || hyprshot -m ${1:-region} --raw |
+#   satty --filename - \
+#     --output-filename "/tmp/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png" \
+#     --early-exit \
+#     --actions-on-enter save-to-clipboard \
+#     --copy-command 'wl-copy'
+#
+#
+
+flameshot gui
