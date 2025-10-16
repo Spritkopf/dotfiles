8cce1ec1415c81574a04ee393c25c4b30ba51791
Ricardo Beck <spritkopf@gmail.com>
2025-10-16 11:27:44 +0200

    update hyprland config

--- general/install_tools.sh
+++ general/install_tools.sh
@@ -2,12 +2,4 @@
 # some helpful tools I might need
 
 
-sudo pacman -S --noconfirm --needed \
-    lazygit \ # simply the GOAT
-    bat \    # better cat
-    dust \   # better du: find large files
-    bottom \ # better htop
-    fzf \    # fuzzy finder
-    ripgrep \ # fast recursive grep
-    tealdeer \ # fast tldr replacement
-    ouch \   # Compression / Decompression of various formats
+sudo pacman -S --noconfirm --needed lazygit bat    dust   bottom fzf ripgrep tealdeer ouch   
--- install_hyprland_packages.sh
+++ install_hyprland_packages.sh
@@ -1,3 +1,3 @@
 # Install all necessary packages for the Hyprland config
 #
-sudo pacman -S hyprland hyprlock hyprpaper waybar hyprshot satty
+sudo pacman -S hyprland hyprlock hyprpaper waybar flameshot atuin
