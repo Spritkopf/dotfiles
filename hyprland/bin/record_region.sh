ccb20617cc368b6570e7035317f4c61d11113970
Ricardo Beck <spritkopf@gmail.com>
2026-02-19 07:51:08 +0100

    record_region: convert to gif

--- hyprland/bin/record_region.sh
+++ hyprland/bin/record_region.sh
@@ -3,4 +3,8 @@
 pgrep -x "wf-recorder" && pkill -INT -x wf-recorder && notify-send -h string:wf-recorder:record -t 1000 "Finished Recording" && exit 0
 
 dateTime=$(date +%m-%d-%Y-%H:%M:%S)
-wf-recorder --bframes max_b_frames -f $HOME/Videos/$dateTime.mp4 -g "$(slurp)"
+filename=$HOME/Videos/$dateTime
+
+wf-recorder --bframes max_b_frames --framerate 20 -f $filename.mp4 -g "$(slurp)" && \
+ffmpeg -i $filename.mp4 -vf "fps=20" $filename.gif
+
