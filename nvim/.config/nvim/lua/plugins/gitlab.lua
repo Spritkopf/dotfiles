638ff29c26614c11a485e0f02c79a82984199e64
Ricardo Beck <spritkopf@gmail.com>
2026-03-04 09:32:30 +0100

    update...

--- nvim/.config/nvim/lua/plugins/gitlab.lua
+++ nvim/.config/nvim/lua/plugins/gitlab.lua
@@ -5,12 +5,14 @@ return {
             "MunifTanjim/nui.nvim",
             "nvim-lua/plenary.nvim",
             "sindrets/diffview.nvim",
-            "stevearc/dressing.nvim",                           -- Recommended but not required. Better UI for pickers.
-            "nvim-tree/nvim-web-devicons",                      -- Recommended but not required. Icons in discussion tree.
+            "stevearc/dressing.nvim",                                -- Recommended but not required. Better UI for pickers.
+            "nvim-tree/nvim-web-devicons",                           -- Recommended but not required. Icons in discussion tree.
         },
         build = function() require("gitlab.server").build(true) end, -- Builds the Go binary
         config = function()
-            require("gitlab").setup()
+            require("gitlab").setup({
+                config_path = vim.env.HOME .. "/secret"
+            })
         end,
     }
 }
