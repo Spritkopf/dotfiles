61737898e634c0ea55917a40d0ede82e717dec06
Ricardo Beck <spritkopf@gmail.com>
2026-06-15 10:58:30 +0200

    copilot tab binding fix

--- nvim/.config/nvim/lsp/toml.lua
+++ nvim/.config/nvim/lsp/toml.lua
@@ -3,4 +3,5 @@ return {
     filetypes = { 'toml' },
     -- single_file_support = true,
     root_dir = './../',
+    
 }
--- nvim/.config/nvim/lua/plugins/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/codecompanion.lua
@@ -61,6 +61,17 @@ return {
 
             vim.keymap.set("n", "<leader>ct", function() require("copilot.suggestion").toggle_auto_trigger() end,
                 { desc = "Toggle Auto-[T]riggering of suggestions" })
+            vim.keymap.set("i", "<Tab>", function()
+                if require("copilot.suggestion").is_visible() then
+                    require("copilot.suggestion").accept()
+                else
+                    vim.api.nvim_feedkeys(
+                        vim.api.nvim_replace_termcodes("<Tab>", true, false, true),
+                        "n",
+                        false
+                    )
+                end
+            end, { desc = "Accept Copilot suggestion or Tab" })
         end,
     },
 }
