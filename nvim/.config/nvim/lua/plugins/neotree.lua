b871b218d5902ad284aa4dbea739c806febb74b3
Ricardo Beck <spritkopf@gmail.com>
2026-07-02 08:44:47 +0200

    neotree: add window picker

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,12 +1,16 @@
-return{
+return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons",
         "MunifTanjim/nui.nvim",
+        "s1n7ax/nvim-window-picker",
     },
     config = function()
-        vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', { desc = "[E]xplorer (NeoTree)"})
+        vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', { desc = "[E]xplorer (NeoTree)" })
+
+        require 'window-picker'.setup({
+            hint = 'floating-letter', })
     end
 }
